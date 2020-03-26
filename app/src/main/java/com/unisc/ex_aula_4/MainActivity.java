ba0093936f3cbd4e01fcd20d199ec329a436b71c
gabrielquadro <61526044+gabrielquadro@users.noreply.github.com>
2020-03-26 20:55:55 -0300

    Desenvolvimento layout

---
+++ app/src/main/java/com/unisc/ex_aula_4/MainActivity.java
@@ -0,0 +1,37 @@
+package com.unisc.ex_aula_4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void cliquePicIt(View view) {
+    }
+
+    public void cliqueMaps3(View view) {
+    }
+
+    public void cliqueMaps2(View view) {
+    }
+
+    public void cliqueMaps1(View view) {
+    }
+
+    public void cliqueCall(View view) {
+    }
+
+    public void cliqueWeb(View view) {
+    }
+
+    public void cliqueContatos(View view) {
+    }
+}
